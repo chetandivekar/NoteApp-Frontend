faba874b0780f9666830cc9ec8053a4687a6a42d
chetandivekar <chetandivekar6@gmail.com>
2023-06-15 23:38:44 +0530

    About updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,26 @@
-import React, { useContext } from "react";
-import noteContext from "../contexts/notes/noteContext";
+import React from "react";
 
 export default function About() {
-  const a = useContext(noteContext);
   return (
     <div>
-      About {a.name} and he is in the class {a.class}
+      <h2>About Note App</h2>
+      <p>
+        Note App is a simple and intuitive application that allows you to
+        create, manage, and organize your personal notes. It provides an
+        easy-to-use interface for creating new notes, editing existing ones, and
+        deleting notes when they are no longer needed.
+      </p>
+      <p>
+        With Note App, you can keep track of important information, jot down
+        ideas, make to-do lists, and more. It's a convenient tool for staying
+        organized and ensuring that your thoughts and tasks are easily
+        accessible.
+      </p>
+      <p>
+        Note App is built using React and designed to be responsive, making it
+        accessible from various devices and screen sizes. Feel free to explore
+        the app and start managing your notes today!
+      </p>
     </div>
   );
 }
